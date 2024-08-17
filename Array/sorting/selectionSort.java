c0adff17129b29ed4871d210c041d30a9e94e582
jahnavee45 <roysimran1545@gmail.com>
2024-08-17 11:04:19 +0530

    new commit

--- Array/sorting/selectionSort.java
+++ Array/sorting/selectionSort.java
@@ -1,22 +1,22 @@
 package sorting;
 
-public class bubbleSort {
+public class selectionSort {
 
-    static void sortOperation(int[] arr, int n) {
-        int temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+    static void sortOperation(int[] arr, int n){
+        for(int i = 0; i < n; i++){
+            int min_ele = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[min_ele] > arr[j]){
+                    min_ele = j;
                 }
             }
+            int temp = arr[min_ele];
+            arr[min_ele] = arr[i];
+            arr[i] = temp;
         }
     }
-
     public static void main(String[] args) {
-        int[] arr = { 3, 2, 5, 7, 1, 8, 6, 4};
+        int[] arr = { 3, 2, 5, 7, 1, 8, 6, 4 };
         int n = arr.length;
 
         System.out.print("Before sorting: ");
@@ -30,5 +30,4 @@ public class bubbleSort {
             System.out.print(arr[i] + " ");
         }
     }
-
 }
