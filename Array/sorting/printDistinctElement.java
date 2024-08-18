425b432f673c942bd7efbf3e92f208da9f70f73b
jahnavee45 <roysimran1545@gmail.com>
2024-08-18 16:55:48 +0530

    new commit

--- Array/sorting/printDistinctElement.java
+++ Array/sorting/printDistinctElement.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class printDistinctElement {
     static void findDistinct(int[] arr, int n){
         HashMap<Integer, Integer> map = new HashMap<>();
-        System.out.print("\nAfter eleminating distinct element: ");
+        System.out.print("\nDistinct element after eleminating: ");
         for(int i = 0; i < n; i++){
             if(!map.containsKey(arr[i])){
                 map.put(arr[i], i);
@@ -14,21 +14,26 @@ public class printDistinctElement {
         }
     }
 
-    // static void findDistinctSorting(int[] arr, int n){
-    //     System.out.print("\nAfter eleminating distinct element: ");
-    //     for(int i = 0; i < n; i++){
-    //         int min_ele = i;
-    //         for(int j = i+1; j < n; j++){
-    //             if(arr[min_ele] > arr[j] || arr[min_ele] != arr[j]){
-    //                 min_ele = j;
-    //             }
-    //         }
-    //         int temp = arr[min_ele];
-    //         arr[min_ele] = arr[i];
-    //         arr[i] = temp;
-    //         System.out.print(arr[i] + " ");
-    //     }
-    // }
+    static void findDistinctSorting(int[] arr, int n){
+        System.out.print("\nDistinct element after sorting & eleminating: ");
+        for(int i = 0; i < n; i++){
+            int min_ele = i;
+            for(int j = i+1; j < n; j++){
+                if(arr[min_ele] > arr[j]){
+                    min_ele = j;
+                }
+            }
+            int temp = arr[min_ele];
+            arr[min_ele] = arr[i];
+            arr[i] = temp;
+        }
+
+        for(int i = 1; i < n; i++){
+            if(arr[i] != arr[i-1]){
+                System.out.print(arr[i-1] + " ");
+            }
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 1, 3, 4, 4, 5};
         int n = arr.length;
@@ -38,7 +43,7 @@ public class printDistinctElement {
             System.out.print(arr[i] + " ");
         }
         findDistinct(arr, n);
-        // findDistinctSorting(arr, n);
+        findDistinctSorting(arr, n);
     }
     
 }
