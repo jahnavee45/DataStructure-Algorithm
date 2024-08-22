c890b4d627a1b7d1eef1baa2a7b2af0b1920d874
jahnavee45 <roysimran1545@gmail.com>
2024-08-22 10:54:30 +0530

    new commit

---
+++ Array/sortAbsoluteDiiference.java
@@ -0,0 +1,35 @@
+public class sortAbsoluteDiiference {
+
+    static void sortOperation(int[] arr, int n, int x){
+        int[] arr2 = new int[n];
+        for(int i = 0; i < n; i++){
+            arr2[i] = Math.abs(x - arr[i]);
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-1; j++){
+                if(arr2[j] > arr2[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 4, 9, 3};
+        int n = arr.length;
+        int diff = 7;
+
+        System.out.print("Before sorting: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        sortOperation(arr, n, diff);
+        System.out.print("\nAfter sorting: ");
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
