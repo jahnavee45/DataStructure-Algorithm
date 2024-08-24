3e6963abbb2f6e2ba31368d1f594bb9e7674263e
jahnavee45 <roysimran1545@gmail.com>
2024-08-24 10:37:19 +0530

    new commit

---
+++ Array/countInversions.java
@@ -0,0 +1,23 @@
+public class countInversions {
+
+    static int findCount(int[] arr, int n){
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(arr[i] > arr[j]){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 5};
+        int n = arr.length;
+
+        int res = findCount(arr, n);
+        System.out.println("The number of inversions in the given array is " + res);
+    }
+}
