ae920093129c50c0ffedde5311c5ce10cbee8220
jahnavee45 <roysimran1545@gmail.com>
2024-08-25 12:39:38 +0530

    new commit

---
+++ Array/printTwoSum.java
@@ -0,0 +1,26 @@
+public class printTwoSum {
+
+    static int findTwoSum(int[] a, int n, int k){
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(a[i] - k == a[j]){
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] a = {0, -1, 2, -3, 1};
+        int n = a.length;
+        int k = -2;
+
+        int i = findTwoSum(a, n, k);
+        if(i == 0){
+            System.out.println("No");
+        }
+        else{
+            System.out.println("Yes");
+        }
+    }
+}
