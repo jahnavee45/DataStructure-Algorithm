334e763c4baa1be98fae7084aa3794e595c41af6
jahnavee45 <roysimran1545@gmail.com>
2024-08-26 11:59:00 +0530

    new commit

---
+++ Array/oddOccurence.java
@@ -0,0 +1,19 @@
+public class oddOccurence {
+
+    static int findOddCount(int[] a, int n){
+        int count = 0;
+        for(int  i = 0; i < n; i++){
+            if(a[i] % 2 != 0 && a[i] > 1){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] a = {5, 7, 2, 7, 5, 2, 5};
+        int n = a.length;
+
+        int res = findOddCount(a, n);
+        System.out.println("The number of odd occurring number is " + res);
+    }
+}
