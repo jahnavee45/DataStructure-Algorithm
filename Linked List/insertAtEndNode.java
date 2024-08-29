d9b912fced174d78d73e78d3304410a41b81cff4
jahnavee45 <roysimran1545@gmail.com>
2024-08-29 12:02:36 +0530

    new commit

---
+++ Linked List/insertAtEndNode.java
@@ -0,0 +1,32 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
+public class insertAtEndNode {
+
+    static void printList(Node head){
+        Node curr = head;
+
+        while(curr!= null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+    public static void main(String[] args) {
+
+        Node head = new Node(3);
+        head.next = new Node(5);
+        head.next.next = new Node(8);
+        head.next.next.next = new Node(10);
+
+        System.out.print("Before insertion: ");
+        printList(head);
+        
+    }
+}
