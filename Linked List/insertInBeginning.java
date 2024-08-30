55885056054cad93286e77e5f352d1b47621cdb7
jahnavee45 <roysimran1545@gmail.com>
2024-08-30 11:45:59 +0530

    new commit

--- Linked List/insertInBeginning.java
+++ Linked List/insertInBeginning.java
@@ -3,38 +3,28 @@ class Node{
     Node next;
 
     Node(int new_data){
-        data = new_data;
-        next = null;
+        this.data = new_data;
+        this.next = null;
     }
 }
 
 public class insertInBeginning {
 
-    static Node insertNode(Node head, int data){
-        Node new_node = new Node(data);
-        new_node.next = head;
-        return new_node;
-    }
-
-    static void printNode(Node head){
+    static void printList(Node head){
         Node curr = head;
+
         while(curr != null){
             System.out.print(curr.data + " ");
             curr = curr.next;
         }
     }
     public static void main(String[] args) {
-        Node head = new Node(2);
+        Node head  = new Node(2);
         head.next = new Node(3);
         head.next.next = new Node(4);
         head.next.next.next = new Node(5);
 
-        System.out.print("Before node insertion: ");
-        printNode(head);
-
-        System.out.print("\nAfter node insertion: ");
-        int data = 1;
-        head = insertNode(head, data);
-        printNode(head);
+        System.out.print("Before insertion: ");
+        printList(head);
     }
 }
