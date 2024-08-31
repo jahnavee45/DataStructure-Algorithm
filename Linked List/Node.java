3b9e553f062d030919e1ad6a608c0fef526eb367
jahnavee45 <roysimran1545@gmail.com>
2024-08-31 15:10:33 +0530

    new commit

--- Linked List/Node.java
+++ Linked List/Node.java
@@ -6,5 +6,19 @@ public class Node{
         data = new_data;
         next = null;
     }
+
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+
+        System.out.print("Traverse an list: ");
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
 }
 
