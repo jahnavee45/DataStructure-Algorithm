a77ec8359489132c38f81b5ad9431f97527dab0e
jahnavee45 <roysimran1545@gmail.com>
2024-09-02 11:20:11 +0530

    new commit

--- Linked List/deleteHeadNode.java
+++ Linked List/deleteHeadNode.java
@@ -10,6 +10,15 @@ class Node{
 
 public class deleteHeadNode {
 
+    static Node deletehead(Node head){
+        if (head == null){
+            return null;
+        }
+
+        head = head.next;
+
+        return head;
+    }
     static void printList(Node head){
         while(head != null){
             System.out.print(head.data + " ");
@@ -18,12 +27,16 @@ public class deleteHeadNode {
     }
 
     public static void main(String[] args) {
-        Node head = new Node(1);
+        Node head = new Node(5);
         head.next = new Node(2);
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
 
         System.out.print("Before deletion of head node: ");
+        printList(head);
+
+        head = deletehead(head);
+        System.out.print("\n After deletion of head node: ");
         printList(head);
     }
     
