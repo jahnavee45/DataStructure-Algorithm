647db15f890d2c09e858222cce07a57c99a7308b
jahnavee45 <roysimran1545@gmail.com>
2024-09-10 11:04:37 +0530

    new commit

---
+++ Linked List/reverseList.java
@@ -0,0 +1,29 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class reverseList {
+
+    static void printList(Node head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(4);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(8);        
+        head.next.next.next.next = new Node(10);     
+        
+        System.out.print("Before reversing: ");
+        printList(head);
+    }
+}
