6248be8506e6e4c85c4b6203392dc7c76dc78b5d
jahnavee45 <roysimran1545@gmail.com>
2024-09-11 12:19:50 +0530

    new commit

---
+++ Loops/whileLoop.java
@@ -0,0 +1,19 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String answer = "yes";
+        while(answer.equals("yes")){
+            System.out.println("Enter an amount: ");
+            int amount = sc.nextInt();
+            System.out.println("Cash withdraw successful!" + amount);
+            System.out.println("Do you want to continue? yes/no");
+            answer = sc.next();
+        }
+        System.out.println("Vist again!");
+    }
+}
