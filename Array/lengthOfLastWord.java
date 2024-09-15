e3ec190b5ab2b0615791088c07784d8697f78126
jahnavee45 <roysimran1545@gmail.com>
2024-09-15 11:46:44 +0530

    new commit

--- Array/lengthOfLastWord.java
+++ Array/lengthOfLastWord.java
@@ -1,8 +1,9 @@
 public class lengthOfLastWord {
 
-    static int countLength(String str, int n){
+    static int countLength(String str){
         int count = 0;
-        int i = n-1;
+        str = str.trim();
+        int i = str.length()-1;
         while(str.charAt(i) != ' '){
             count++;
             i--;
@@ -10,9 +11,8 @@ public class lengthOfLastWord {
         return count;
     }
     public static void main(String[] args) {
-        String str = "This is an example";
-        int n = str.length();
-        int res = countLength(str, n);
+        String str = " This is an example ";
+        int res = countLength(str);
         System.out.println("The length of the last word is " + res);
     }
 }
