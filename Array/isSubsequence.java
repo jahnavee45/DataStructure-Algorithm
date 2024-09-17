fb96f5c916632b3be9a580fb7f613e45dd205e1b
jahnavee45 <roysimran1545@gmail.com>
2024-09-17 11:52:15 +0530

    new commit

---
+++ Array/isSubsequence.java
@@ -0,0 +1,30 @@
+public class isSubsequence {
+
+    static boolean checkIfPresent(String s, String c){
+        int i = 0, j = 0;
+
+        while(i < c.length()){
+            if(c.charAt(i) == s.charAt(j)){
+                j++;
+            }
+            i++;
+        }
+
+        if(j != s.length()){
+            return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "b";
+        String c = "adbfc";
+        boolean res = checkIfPresent(s, c);
+
+        if(res == true){
+            System.out.println("Subsequence is present");
+        }else{
+            System.out.println("Subsequence is not present");
+        }
+    }
+}
