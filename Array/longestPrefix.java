432997063bfca5dc271d9f30932169d4c7aa8227
jahnavee45 <roysimran1545@gmail.com>
2024-09-18 11:06:31 +0530

    new commit

---
+++ Array/longestPrefix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class longestPrefix {
+
+    static String findPrefix(String[] s){
+        StringBuffer st = new StringBuffer();
+
+        Arrays.sort(s);
+        String start = s[0];
+        String end = s[s.length - 1];
+
+        for(int i = 0; i < Math.min(start.length(), end.length()); i++){
+            if(start.charAt(i) != end.charAt(i)){
+                return st.toString();
+            }
+            st.append(start.charAt(i));
+        }
+        return st.toString();
+    }
+    public static void main(String[] args) {
+        String[] s = {"flower", " flow", " flight"};
+
+        String res = findPrefix(s);
+        System.out.println("The longest common prefix is " + res);
+    }
+}
