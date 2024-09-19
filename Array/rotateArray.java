657ef8892a552367c60a8049cbe9989cf12818f6
jahnavee45 <roysimran1545@gmail.com>
2024-09-19 10:28:59 +0530

    new commit

--- Array/rotateArray.java
+++ Array/rotateArray.java
@@ -2,6 +2,26 @@
 
 public class rotateArray {
 
+    static void reverse(int[] a, int i, int j){
+
+        while(i < j){
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    static void rotate(int[] a, int k){
+
+        k %= a.length;
+
+        reverse(a, 0, a.length-1);
+        reverse(a, 0, k-1);
+        reverse(a, k, a.length-1);
+    }
+
     static void printArray(int[] a){
         for(int i : a){
             System.out.print(i + " ");
@@ -12,6 +32,10 @@ public class rotateArray {
         int k = 2;
 
         System.out.print("Before rotating: ");
+        printArray(a);
+
+        System.out.print("\nAfter rotating: ");
+        rotate(a, k);
         printArray(a);
     }
 }
