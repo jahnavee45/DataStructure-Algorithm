5c17979cb5af83720ecdb3d22bce950d01cb0f50
jahnavee45 <roysimran1545@gmail.com>
2024-09-27 11:25:24 +0530

    new commit

---
+++ Linked List/findMissingNumber.java
@@ -0,0 +1,19 @@
+public class findMissingNumber {
+
+    static int findNumber(int[] arr){
+        int tsum = 10 * (10 + 1) / 2;
+        int asum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            asum += arr[i];
+        }
+
+        return tsum - asum;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Missing number is " + findNumber(arr));
+
+    }   
+}
