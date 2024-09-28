013b29f59a643f06a283a9cdc4c25d5bd827b429
jahnavee45 <roysimran1545@gmail.com>
2024-09-28 11:14:24 +0530

    new commit

--- Linked List/findMissingNumber.java
+++ Linked List/findMissingNumber.java
@@ -13,7 +13,7 @@ public class findMissingNumber {
     public static void main(String[] args) {
         int[] arr = {1, 2, 4, 5, 6, 7, 8, 9, 10};
 
-        System.out.println("Missing number is " + findNumber(arr));
+        System.out.println("Missing number: " + findNumber(arr));
 
     }   
 }
