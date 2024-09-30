c04c97724e873f6c383f5fa326da29e47c6ea7d6
jahnavee45 <roysimran1545@gmail.com>
2024-09-30 10:48:08 +0530

    new commit

--- Linked List/rotateList.java
+++ Linked List/rotateList.java
@@ -9,7 +9,23 @@ class Node{
 }
 
 public class rotateList {
+
+    static void printList(Node head){
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+    }
     public static void main(String[] args) {
-        
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
+        head.next.next.next = new Node(5);
+        head.next.next.next.next = new Node(6);
+        head.next.next.next.next.next = new Node(7);
+
+        System.out.print("Before rotating the list: ");
+        printList(head);
+
     }
 }
