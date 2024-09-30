d52167758f7da58bc267b67df43fdbc826a0dc14
jahnavee45 <roysimran1545@gmail.com>
2024-09-30 10:45:39 +0530

    new commit

---
+++ Linked List/rotateList.java
@@ -0,0 +1,15 @@
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class rotateList {
+    public static void main(String[] args) {
+        
+    }
+}
