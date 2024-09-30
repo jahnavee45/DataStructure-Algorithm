33e2568586a2e84d1ac3b1f2a66603f2d6309825
jahnavee45 <roysimran1545@gmail.com>
2024-09-30 11:03:25 +0530

    new commit

--- Linked List/rotateList.java
+++ Linked List/rotateList.java
@@ -10,6 +10,26 @@ class Node{
 
 public class rotateList {
 
+    static Node rotateOperation(Node head, int k){
+
+        if(k == 0 || head == null){
+            return head;
+        }
+
+        for(int i = 0; i < k; ++i){
+            Node curr = head;
+            while(curr.next != null){
+                curr = curr.next;
+            }
+
+            curr.next = head;
+            curr = curr.next;
+            head = head.next;
+            curr.next = null;
+        }
+        return head;
+    }
+
     static void printList(Node head){
         while(head != null){
             System.out.print(head.data + " ");
@@ -17,14 +37,17 @@ public class rotateList {
         }
     }
     public static void main(String[] args) {
-        Node head = new Node(2);
-        head.next = new Node(3);
-        head.next.next = new Node(4);
-        head.next.next.next = new Node(5);
-        head.next.next.next.next = new Node(6);
-        head.next.next.next.next.next = new Node(7);
+        Node head = new Node(20);
+        head.next = new Node(30);
+        head.next.next = new Node(40);
+        head.next.next.next = new Node(50);
 
         System.out.print("Before rotating the list: ");
+        printList(head);
+
+        int k = 1;
+        System.out.print("\nAfter rotating the list: ");
+        rotateOperation(head, k);
         printList(head);
 
     }
