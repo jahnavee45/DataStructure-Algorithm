013c8f6c1b8ab9f19270117bd5c3334f21e17638
jahnavee45 <roysimran1545@gmail.com>
2024-10-03 11:57:05 +0530

    new commit

---
+++ Recursion/factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial of: ");
+        int n = sc.nextInt();
+
+        System.out.println("The factorial of " + n + " is " + fact(n));
+            }
+        
+            private static int fact(int n) {
+                if(n == 0){
+                    return 1;
+                }
+
+                return n * fact(n-1);
+            }
+}
