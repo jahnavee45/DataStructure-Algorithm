c116564200b6cf5f86bb3e9e8246e2622451b50e
jahnavee45 <roysimran1545@gmail.com>
2024-10-04 12:35:07 +0530

    new commit

---
+++ String/pallindromeString.java
@@ -0,0 +1,27 @@
+package String;
+
+public class pallindromeString {
+    public static void main(String[] args) {
+        String s = "abbc";
+
+        int res = checkPallindrome(s);
+        if (res == 0) {
+            System.out.println("Is pallindrome");
+        } else {
+            System.out.println("Is not pallindrome");
+        }
+    }
+
+    private static int checkPallindrome(String s) {
+        String c = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            c += s.charAt(i);
+        }
+
+        if (c.equals(s)) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
