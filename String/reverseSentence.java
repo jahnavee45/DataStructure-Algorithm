b456c7aae4c4b0f41a61e07a5e337c2b50cd09b8
jahnavee45 <roysimran1545@gmail.com>
2024-10-08 11:32:25 +0530

    new commit

---
+++ String/reverseSentence.java
@@ -0,0 +1,23 @@
+package String;
+
+public class reverseSentence {
+    public static void main(String[] args) {
+        String s = "sky is blue";
+        System.out.println("Before reversing:" + s);
+        System.out.println("After reversing:" + findReverse(s));
+            }
+        
+            private static String findReverse(String s) {
+                String[] str = s.split("\\s+");
+                StringBuilder res = new StringBuilder();
+
+                for(int i = str.length - 1; i >= 0; i--){
+                    res.append(str[i]);
+                    if(i != 0){
+                        res.append(" ");
+                    }
+                }
+
+                return res.toString().trim();
+            }
+}
