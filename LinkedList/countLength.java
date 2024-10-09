b20d208a002e6fedbaaa17fa825b5bee19bf2d39
jahnavee45 <roysimran1545@gmail.com>
2024-10-09 10:51:36 +0530

    new commit

--- LinkedList/countLength.java
+++ LinkedList/countLength.java
@@ -1,54 +1,30 @@
 package LinkedList;
 
-class Node{
+class Node {
     int data;
     Node next;
 
-    Node(int data){
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
 
 public class countLength {
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.next = new Node(4);
 
-    static int totalLengthRecur(Node head){
-        if(head == null){
-            return 0;
-        }
-
-        return 1 + totalLengthRecur(head.next);
+        System.out.println("The length of given linked list is " + findLength(head));
     }
 
-    static int totalLength(Node head){
+    private static int findLength(Node head) {
         int count = 0;
         while (head != null) {
-            head = head.next;
             count++;
+            head = head.next;
         }
         return count;
-    }
-
-    static void  printList(Node head){
-        while (head != null) {
-            System.out.print(head.data + " ");
-            head = head.next;
-        }
-    }
-    public static void main(String[] args) {
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-
-        System.out.print("Printing list: ");
-        printList(head);
-
-        // iterative method to find length of list
-        int count = totalLength(head);
-        System.out.print("\n Total length of the list using iterative method: " + count);
-
-        // recursive method to find length of list
-        System.out.print("\n Total length of the list using recursion: " + totalLengthRecur(head));
     }
 }
