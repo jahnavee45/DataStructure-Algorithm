30759e1cb17f23e4322ef556c5746114cafb9fcb
jahnavee45 <roysimran1545@gmail.com>
2024-10-15 11:03:01 +0530

    new commit

--- pattern/reverseTriangle.java
+++ pattern/reverseTriangle.java
@@ -11,15 +11,5 @@ public class reverseTriangle {
             }
             System.out.println();
         }
-
-        for(int i = 0; i < 5; i++){
-            for(int j = 0; j <= 2*(5 - i); j++){
-                System.out.print(" ");
-            }
-            for(int k = 0; k <= 5-i-1; k++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
     }
 }
