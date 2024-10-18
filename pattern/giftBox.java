71afcd906198025c3a3b8e5d09052a3539a99e6b
jahnavee45 <roysimran1545@gmail.com>
2024-10-18 10:40:20 +0530

    new commit

---
+++ pattern/giftBox.java
@@ -0,0 +1,23 @@
+public class giftBox {
+    public static void main(String[] args) {
+        int i, j, n = 5;
+
+        for(i = 0; i < n; i++){
+            if(i == 0 || i == 2 || i == n-1){
+                for(j = 0; j < n; j++){
+                    System.out.print("* ");
+                }               
+            }else{
+                for(j = 0; j < n; j++){
+                    if(j == 0 || j == 2 || j == n-1){
+                        System.out.print("* ");
+                    }else{
+                        System.out.print("  ");
+                    }
+                }  
+            }
+            System.out.println();
+            
+        }
+    }
+}
