09ee2e19a09a556cc8ea1e4f5278359c6625d906
jahnavee45 <roysimran1545@gmail.com>
2024-10-21 11:19:10 +0530

    new commit

--- GeneralUseCase/findSquareRoot.java
+++ GeneralUseCase/findSquareRoot.java
@@ -8,29 +8,27 @@ public class findSquareRoot {
         System.out.println("Enter the number to find the square root of: ");
         int num = sc.nextInt();
         System.out.println("The square root of " + num + " is " + findRoot(num));
-                sc.close();
+        sc.close();
+    }
+
+    private static int findRoot(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+        int start = 1, end = num, mid = -1;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (mid * mid == num) {
+                return mid;
+            } else if (mid * mid > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-        
-            private static int findRoot(int num) {
-                if(num == 0){
-                    return 0;
-                }
-                if(num == 1){
-                    return 1;
-                }
-                int start = 1, end = num, mid = -1;
-                while(start <= end){
-                    mid = start + (end - start) / 2;
-                    if(mid * mid == num){
-                        return mid;
-                    }
-                    else if(mid * mid > num){
-                        end = mid - 1;
-                    }
-                    else{
-                        start = mid + 1;
-                    }
-                }
-                return Math.round(end);
-            }
+        }
+        return Math.round(end);
+    }
 }
