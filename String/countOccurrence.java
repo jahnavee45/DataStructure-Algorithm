95c7ebc523ceabde70b2ef7aaf7d6791189a9b4b
jahnavee45 <roysimran1545@gmail.com>
2024-10-23 15:34:31 +0530

    new commit

--- String/countOccurrence.java
+++ String/countOccurrence.java
@@ -5,9 +5,16 @@ public class countOccurrence {
         String str = "A cat sat on the mat";
         char ch = 'a';
         System.out.println("The total number of " + ch + " present in the given string is " + totalCount(str, ch));
+    }
+
+    private static int totalCount(String str, char ch) {
+        int count = 0;
+        str = str.toLowerCase();
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == ch){
+                count++;
             }
-        
-            private static int totalCount(String str, char ch) {
-                
-             }
+        }
+        return count;
+    }
 }
