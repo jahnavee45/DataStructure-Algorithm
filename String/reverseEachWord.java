4264c55f75dccddcca75fd1453c65c1281e2ecc2
jahnavee45 <roysimran1545@gmail.com>
2024-10-25 11:18:32 +0530

    new commit

---
+++ String/reverseEachWord.java
@@ -0,0 +1,18 @@
+package String;
+
+public class reverseEachWord {
+    public static void main(String[] args) {
+        String str = "sky is blue";
+        System.out.println("Before reversing: " + str);
+        System.out.println("After reversing: " + reverseOperation(str));
+    }
+
+    private static String reverseOperation(String str) {
+        StringBuilder res = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++){
+            
+        }
+    }
+}
