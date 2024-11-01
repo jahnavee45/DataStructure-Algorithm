9ea6665e3c7a6871f413e8f0dfbc3de864b18a46
jahnavee45 <roysimran1545@gmail.com>
2024-11-01 11:35:17 +0530

    new commit

--- GeneralUseCase/methodRefrence.java
+++ GeneralUseCase/methodRefrence.java
@@ -4,21 +4,16 @@ interface Welcome {
     void print();
 }
 
-class Rectangle{
-    int a; int b;
-    void area(int a, int b){
-        this.a = a;
-        this.b = b;
-        System.out.println("Area of rectangle: " + a*b);
+class HelloWorld{
+    void printHello(){
+        System.out.println("Hello World!");
     }
 }
 class methodRefrence {
      public static void main(String args[])
     {
-        Rectangle rec = new Rectangle();
-        rec.area(23, 12);
         int a = 5;
-        Welcome w = rec::area;
+        Welcome w = HelloWorld::printHello();
         w.print();
     }
 }
