b548e499a4daeabef80a275c76a3e63b3a2f7846
jahnavee45 <roysimran1545@gmail.com>
2024-11-05 15:58:08 +0530

    new commit

--- pattern/numbers.java
+++ pattern/numbers.java
@@ -8,6 +8,12 @@ public class numbers {
             System.out.println();
         }
 
-        // for(int i = 1; i <= n; i++)
+        int k = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(k++ + " ");
+            }
+            System.out.println();
+        }
     }
 }
