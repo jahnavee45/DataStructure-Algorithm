07634a252c426f6db9187916c353bd516effe650
jahnavee45 <roysimran1545@gmail.com>
2024-11-05 15:59:45 +0530

    new commit

--- pattern/numbers.java
+++ pattern/numbers.java
@@ -1,6 +1,7 @@
 public class numbers {
     public static void main(String[] args) {
         int n = 5;
+        System.out.println("Number left triangle:");
         for(int i = 1; i <= n; i++){
             for(int j = 1; j<=i; j++){
                 System.out.print(j + " ");
@@ -9,6 +10,7 @@ public class numbers {
         }
 
         int k = 1;
+        System.out.println("\nIncreasing number pattern triangle:");
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 System.out.print(k++ + " ");
