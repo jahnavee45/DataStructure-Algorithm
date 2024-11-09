573616f16da5ee3b3429390e8e86bad4d7270722
jahnavee45 <roysimran1545@gmail.com>
2024-11-09 16:12:59 +0530

    new commit

---
+++ MapAndSet/hashSetBegin.java
@@ -0,0 +1,16 @@
+package MapAndSet;
+import java.util.HashSet;
+
+public class hashSetBegin {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+
+        // print size of set
+        System.out.println("The size of set is: " + set.size());
+    }
+}
