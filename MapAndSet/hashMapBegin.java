8c47963add00dcaf4720593c79f9f2f30cf775c4
jahnavee45 <roysimran1545@gmail.com>
2024-11-11 15:44:39 +0530

    new commit

--- MapAndSet/hashMapBegin.java
+++ MapAndSet/hashMapBegin.java
@@ -11,6 +11,8 @@ public class hashMapBegin {
         map.put("Grapes", 6);
         map.put("Cherry", 8);
         map.put("Mango", 10);
+
+        // between two same keys, the one declared at last is taken
         map.put("Mango", 20);
 
         // print the size of hashmap
