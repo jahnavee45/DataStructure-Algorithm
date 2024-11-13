7dd2a90b2baaed071cf4b29ccabaea3eca18cea6
jahnavee45 <roysimran1545@gmail.com>
2024-11-13 12:11:48 +0530

    new commit

---
+++ ArrayList/begin.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class begin {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(23);
+        list.add(40);
+        list.add(18);
+        list.add(7);
+        list.add(65);
+        list.add(9);
+
+        System.out.println(list);
+    }
+}
