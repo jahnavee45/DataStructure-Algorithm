7467cbb49c5e228e552dac733ac8ebe545ee61ce
jahnavee45 <roysimran1545@gmail.com>
2024-11-15 12:42:27 +0530

    new commit

--- ArrayList/dynamicCrud.java
+++ ArrayList/dynamicCrud.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class addElementsDynamically {
+public class dynamicCrud {
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
