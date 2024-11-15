464d17ef457ac6c33e5832c377902df390e9a224
jahnavee45 <roysimran1545@gmail.com>
2024-11-15 12:36:51 +0530

    new commit

---
+++ ArrayList/addElementsDynamically.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class addElementsDynamically {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements you want in list: ");
+        int n = sc.nextInt();
+        System.out.printf("Now enter %d elements to add in list: ", n);
+
+        for(int i = 0; i < n; i++){
+            a.add(sc.nextInt());
+        }
+
+        System.out.println("The elements of arraylist are: " + a);
+    }
+}
