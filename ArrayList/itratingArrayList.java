5fa70e47167598dd339e6c3f8615e2fca6ba8f44
jahnavee45 <roysimran1545@gmail.com>
2024-11-16 16:09:12 +0530

    new commit

---
+++ ArrayList/itratingArrayList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class itratingArrayList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+
+        System.out.println("Printing list: " + list);
+
+        System.out.println("Printing list by iterating each element:");
+        Iterator<Integer> it = list.iterator();
+    }
+}
