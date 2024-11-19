d970042bf6bbb030939f8d30d6cd69f3f99a91ea
jahnavee45 <roysimran1545@gmail.com>
2024-11-19 18:21:21 +0530

    new commit

---
+++ ArrayList/addMoreThanOneElement.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class addMoreThanOneElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(2);
+        list1.add(6);
+        list1.add(7);
+        list1.add(8);
+        System.out.println("Printing list1: " + list1);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(6);
+        list2.add(7);
+        list2.add(8);
+        System.out.println("Printing list1: " + list2);
+
+    }
+}
