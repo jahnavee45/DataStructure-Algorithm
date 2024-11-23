b8c4c3c76868f6e94d4cad5431953ebf53924c5b
jahnavee45 <roysimran1545@gmail.com>
2024-11-23 10:38:47 +0530

    new commit

---
+++ ArrayList/minMax.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class minMax {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(24);
+        list.add(13);
+        list.add(6);
+        list.add(90);
+        list.add(55);
+        list.add(78);
+
+        System.out.println("Given list is: " + list);
+    }
+}
