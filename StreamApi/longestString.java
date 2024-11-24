96563d8e491014989d34a9b8c0e0edd58d8a52f4
jahnavee45 <roysimran1545@gmail.com>
2024-11-24 11:53:12 +0530

    new commit

---
+++ StreamApi/longestString.java
@@ -0,0 +1,16 @@
+package StreamApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class longestString {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("mango");
+        list.add("grape");
+        list.add("fig");
+
+        System.out.println("List of fruits: " + list);
+    }
+}
