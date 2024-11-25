e2a0d442f4fc4089dfd5defde642761697b68ac5
jahnavee45 <roysimran1545@gmail.com>
2024-11-25 22:07:53 +0530

    new commit

--- StreamApi/duplicate.java
+++ StreamApi/duplicate.java
@@ -1,7 +1,15 @@
 package StreamApi;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class duplicate {
     public static void main(String[] args) {
-        
+        List<Integer> list = Arrays.asList(2, 4, 5, 6, 5, 7, 8, 2, 9);
+        System.out.println("before removing duplicates: " + list);
+
+        List<Integer> newlist = list.stream().distinct().collect(Collectors.toList());
+        System.out.println("after removing duplicates: " + newlist);
     }
 }
