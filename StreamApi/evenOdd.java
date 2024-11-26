b184658c12cc62c530ec71f60b4b28a276ffca9c
jahnavee45 <roysimran1545@gmail.com>
2024-11-26 21:38:59 +0530

    new commit

---
+++ StreamApi/evenOdd.java
@@ -0,0 +1,19 @@
+package StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class evenOdd {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        System.out.println("Given list is: " + list);
+
+        Map<Boolean, List<Integer>> check = list.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+
+        List<Integer> even = check.get(true);
+        List<Integer> odd = check.get(false);
+
+    }
+}
