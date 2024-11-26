0c49c3386d8765b0d11754c6bc703e9602239e63
jahnavee45 <roysimran1545@gmail.com>
2024-11-26 21:32:56 +0530

    new commit

---
+++ StreamApi/intersectionList.java
@@ -0,0 +1,14 @@
+package StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class intersectionList {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(1, 3, 5, 7);
+        List<Integer> list2 = Arrays.asList(2, 3, 4, 7);
+
+        System.out.println("List 1 is: " + list1);
+        System.out.println("List 2 is: " + list2);
+    }
+}
