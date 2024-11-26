7a0efb7b7d8e55dd94336cc0355b8863692cd841
jahnavee45 <roysimran1545@gmail.com>
2024-11-26 21:40:36 +0530

    new commit

--- StreamApi/evenOdd.java
+++ StreamApi/evenOdd.java
@@ -12,6 +12,8 @@ public class evenOdd {
 
         Map<Boolean, List<Integer>> check = list.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
 
+        System.out.println("List after partitioning even and odd: " + check);
+
         List<Integer> even = check.get(true);
         List<Integer> odd = check.get(false);
 
