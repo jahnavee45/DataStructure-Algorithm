142d92b308d6a767432f4b66383aaf5901044507
jahnavee45 <roysimran1545@gmail.com>
2024-11-27 09:36:29 +0530

    new commit

--- ArrayList/MiniBookManagement/Library.java
+++ ArrayList/MiniBookManagement/Library.java
@@ -13,12 +13,13 @@ public class Library {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Select any of the given options: ");
-        System.out.println("1. Get the list of books \n2. Search any book \n3. Add new book and its category \n4. Remove any book");
+        System.out.println(
+                "1. Get the list of books \n2. Search any book \n3. Add new book and its category \n4. Remove any book");
         int option = sc.nextInt();
 
-        switch(option){
-            case 1:{
-                lib.getAllBooks();
+        switch (option) {
+            case 1: {
+                lib.getAllBooks(books);
             }
             case 2: {
                 System.out.println("Give the name of the book: ");
@@ -33,5 +34,26 @@ public class Library {
                 lib.addNewBook(books, name, category);
             }
         }
+    }
+
+    private void getAllBooks(ArrayList<Book> books) {
+        System.out.println("This is the list of books along with category: ");
+        for(Book book : books){
+            System.out.println(book.getBook() + " " + book.getCategory());
+        }
+
+    }
+
+    private void searchBook(ArrayList<Book> books, String name) {
+        for(Book book : books){
+            if(book.getBook().equals(name)){
+                System.out.println(book.getBook() + " " + book.getCategory());
+            }
+        }
+    }
+
+    private void addNewBook(ArrayList<Book> books, String name, String category) {
+        books.add(new Book(name, category));
+        System.out.println("New book added!");
     }
 }
