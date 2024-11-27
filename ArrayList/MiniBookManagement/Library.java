56091a7a9fe59bbfa67013897435a6b02e576140
jahnavee45 <roysimran1545@gmail.com>
2024-11-27 09:32:15 +0530

    new commit

--- ArrayList/MiniBookManagement/Library.java
+++ ArrayList/MiniBookManagement/Library.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Library {
     public static void main(String[] args) {
@@ -9,8 +10,28 @@ public class Library {
         books.add(new Book("AWS", "Cloud"));
 
         Library lib = new Library();
-        
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Select any of the given options: ");
-        System.out.println("1. Search any book \n2. Add new book and its category \n3. Remove any book");
+        System.out.println("1. Get the list of books \n2. Search any book \n3. Add new book and its category \n4. Remove any book");
+        int option = sc.nextInt();
+
+        switch(option){
+            case 1:{
+                lib.getAllBooks();
+            }
+            case 2: {
+                System.out.println("Give the name of the book: ");
+                String name = sc.nextLine();
+                lib.searchBook(books, name);
+            }
+            case 3: {
+                System.out.println("Give the name of the book: ");
+                String name = sc.nextLine();
+                System.out.println("Give the category: ");
+                String category = sc.nextLine();
+                lib.addNewBook(books, name, category);
+            }
+        }
     }
 }
