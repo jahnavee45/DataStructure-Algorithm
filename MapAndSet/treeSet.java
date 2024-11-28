b768c098a4a7fca3846b4c0fea472c0a99f874ab
jahnavee45 <roysimran1545@gmail.com>
2024-11-28 14:25:21 +0530

    new commit

--- MapAndSet/treeSet.java
+++ MapAndSet/treeSet.java
@@ -1,16 +1,21 @@
 package MapAndSet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 
 public class treeSet {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(20);
+        list.add(14);
+        list.add(63);
+        list.add(20);
+        list.add(8);
+        System.out.println("Printing an arraylist: " + list);
+        
         TreeSet<Integer> set = new TreeSet<>();
-        set.add(20);
-        set.add(14);
-        set.add(63);
-        set.add(20);
-        set.add(8);
-
+        set.addAll(list);
         System.out.println("Printing a tree set: " + set);
     }
 }
