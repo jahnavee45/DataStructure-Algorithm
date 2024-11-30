cf5d1c07f5e4eea8d3389aa9fa5ac9e23a2848ae
jahnavee45 <roysimran1545@gmail.com>
2024-11-30 12:21:11 +0530

    new commit

--- StreamApi/primeNumbers.java
+++ StreamApi/primeNumbers.java
@@ -17,9 +17,19 @@ public class primeNumbers {
         if (n <= 1)
             return false;
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0)
-                return false;
+        // for (int i = 2; i <= Math.sqrt(n); i++) {
+        //     if (n % i == 0)
+        //         return false;
+        // }
+
+        int count = 0;
+        for(int i = 2; i <= n; i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        if(count > 2){
+            return false;
         }
         return true;
     }
