f81e8d5bfee4f5057f69a36dddad8408d753fdc3
jahnavee45 <roysimran1545@gmail.com>
2024-11-30 12:18:11 +0530

    new commit

--- StreamApi/primeNumbers.java
+++ StreamApi/primeNumbers.java
@@ -2,15 +2,15 @@ package StreamApi;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class primeNumbers {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(21, 2, 47, 36, 59);
+        System.out.println("Given list is: " + list);
 
         List<Integer> prime = list.stream().filter(n -> isPrime(n)).collect(Collectors.toList());
-        System.out.println(prime);
+        System.out.println("List of prime numbers: " + prime);
     }
 
     private static boolean isPrime(int n) {
