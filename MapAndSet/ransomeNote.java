57961dd97484fa99f263cf3d8189d43ca5786c4a
jahnavee45 <roysimran1545@gmail.com>
2024-12-01 16:02:18 +0530

    new commit

--- MapAndSet/ransomeNote.java
+++ MapAndSet/ransomeNote.java
@@ -1,9 +1,26 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class ransomeNote {
     public static void main(String[] args) {
         String str1 = "aab";
         String str2 = "aa";
-        
+        System.out.println(isRansome(str1, str2)?"Yes":"No");
+    }
+
+    public static boolean isRansome(String str1, String str2){
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < str1.length(); i++){
+            char c = str1.charAt(i);
+
+            if(!map.containsKey(c)){
+                map.put(c, 1);
+            }else{
+                map.put(c, map.get(c) + 1);
+            }
+        }
+        return true;
     }
 }
