ff5320321e578ec70f393ca84aac38e149d925ec
jahnavee45 <roysimran1545@gmail.com>
2024-12-02 17:32:27 +0530

    new commit

---
+++ Array/isomorphic.java
@@ -0,0 +1,26 @@
+public class isomorphic {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "abb";
+
+        System.out.println(isIsomorphic(s, t)?"Is Isomorphic": "Not Isomorphic");
+    }
+
+    public static boolean isIsomorphic(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int m1[]= new int[200];
+        int m2[]= new int[200];
+
+        for(int i = 0; i < s.length(); i++){
+            if(m1[s.charAt(i)] != m2[t.charAt(i)]){
+                return false;
+            }
+            m1[i] = 
+        }
+        
+        return true;
+    }
+}
