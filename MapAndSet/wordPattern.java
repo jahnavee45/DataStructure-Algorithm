080cc9132140585f235dc87b1da04b4739eddfd2
jahnavee45 <roysimran1545@gmail.com>
2024-12-03 17:05:36 +0530

    new commit

--- MapAndSet/wordPattern.java
+++ MapAndSet/wordPattern.java
@@ -1,10 +1,36 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class wordPattern {
     public static void main(String[] args) {
         String p = "abba";
-        String w = "dog cat cat dog";
+        String w = "dog cat cat fish";
 
-        System.out.println("Word Pattern: " + isMatch(p, w)?"matches" : "not match");
+        System.out.println(isMatch(p, w)?"matches":"not match");
+    }
+
+    public static boolean isMatch(String p, String w){
+        w = w.trim();
+        String st[] = w.split(" ");
+        if(p.length() != st.length){
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        for(int i = 0; i < p.length(); i++){
+            if(!map.containsKey(p.charAt(i))){
+                if(!map.containsValue(st[i])){
+                    map.put(p.charAt(i), st[i]);
+                }else{
+                    return false;
+                }
+            }else{
+                if(st[i].compareTo(map.get(p.charAt(i))) != 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
