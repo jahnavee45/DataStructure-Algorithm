de4ad75911f0bf2fa87f25731431187eec71171f
jahnavee45 <roysimran1545@gmail.com>
2024-12-05 18:02:14 +0530

    new commit

---
+++ MapAndSet/isomorphic.java
@@ -0,0 +1,12 @@
+package MapAndSet;
+
+public class isomorphic {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+
+        System.out.println(isIsomorphic(s, t)?"Yes is isomorphic":"No is not isomorphic");
+    }
+    
+    
+}
