e298f27e09abfa05f2c0629493220360b4bbe4c6
jahnavee45 <roysimran1545@gmail.com>
2024-12-05 18:02:51 +0530

    new commit

--- MapAndSet/isomorphic.java
+++ MapAndSet/isomorphic.java
@@ -1,5 +1,7 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class isomorphic {
     public static void main(String[] args) {
         String s = "egg";
@@ -7,6 +9,26 @@ public class isomorphic {
 
         System.out.println(isIsomorphic(s, t)?"Yes is isomorphic":"No is not isomorphic");
     }
-    
+    public static boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                if (!map.get(s.charAt(i)).equals(t.charAt(i))) {
+                    return false;
+                }
+            } else if (map.containsValue(t.charAt(i))) {
+                return false;
+            } else {
+                map.put(s.charAt(i), t.charAt(i));
+            }
+        }
+        return true;
+    }
     
 }
