036857548f34bf7df48558728a3799a10a657314
jahnavee45 <roysimran1545@gmail.com>
2024-12-06 13:53:13 +0530

    new commit

--- MapAndSet/isomorphic.java
+++ MapAndSet/isomorphic.java
@@ -18,7 +18,15 @@ public class isomorphic {
         HashMap<Character, Character> map = new HashMap<>();
 
         for(int i = 0; i < s.length(); i++){
-            if()
+            if(map.containsKey(s.charAt(i))){
+                if(!map.get(s.charAt(i)).equals(t.charAt(i))){
+                    return false;
+                }
+            }else if(map.containsValue(t.charAt(i))){
+                return false;
+            }else{
+                map.put(s.charAt(i), t.charAt(i));
+            }
         }
         return true;
     }
