3778bf8bf4c5f8174a306a481bbed08fcfda168b
jahnavee45 <roysimran1545@gmail.com>
2024-12-07 09:50:11 +0530

    new commit

--- MapAndSet/twoSum.java
+++ MapAndSet/twoSum.java
@@ -12,9 +12,12 @@ public class twoSum {
         }
 
         int targetSum = 9;
-        findIndex(arr, targetSum);
+        arr = findIndex(arr, targetSum);
 
         System.out.print("Index of elements with sum " + targetSum + " is:" );
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 
     public static int[] findIndex(int[] arr, int target){
