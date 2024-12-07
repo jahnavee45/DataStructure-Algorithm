6ac459de425e6b6df17c6132544c248a0c856be3
jahnavee45 <roysimran1545@gmail.com>
2024-12-07 09:43:26 +0530

    new commit

---
+++ MapAndSet/twoSum.java
@@ -0,0 +1,12 @@
+package MapAndSet;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 3, 1};
+
+        System.out.print("Given array is: ");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
