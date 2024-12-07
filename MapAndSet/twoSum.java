c6d12ed745472813da627f12bd74b48b64fe82ff
jahnavee45 <roysimran1545@gmail.com>
2024-12-07 09:49:03 +0530

    new commit

--- MapAndSet/twoSum.java
+++ MapAndSet/twoSum.java
@@ -1,5 +1,7 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class twoSum {
     public static void main(String[] args) {
         int[] arr = {2, 5, 7, 3, 1};
@@ -8,5 +10,25 @@ public class twoSum {
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+
+        int targetSum = 9;
+        findIndex(arr, targetSum);
+
+        System.out.print("Index of elements with sum " + targetSum + " is:" );
+    }
+
+    public static int[] findIndex(int[] arr, int target){
+         HashMap<Integer, Integer> map = new HashMap<>();
+
+         int[] arr1 = new int[2];
+         for(int i = 0; i < arr.length; i++){
+            if(map.containsKey(target - arr[i])){
+                arr1[0] = i;
+                arr1[1] = map.get(target - arr[i]);
+            }
+            map.put(arr[i], i);
+         }
+
+         return arr1;
     }
 }
