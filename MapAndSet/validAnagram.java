1aa1a908df6600ea3c73051096c635f0e2f666fb
jahnavee45 <roysimran1545@gmail.com>
2024-12-08 09:53:25 +0530

    new commit

---
+++ MapAndSet/validAnagram.java
@@ -0,0 +1,10 @@
+package MapAndSet;
+
+public class validAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t)? "Yes":"No");
+    }
+}
