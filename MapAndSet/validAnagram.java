c8569999b759c3bc43d455ce04e416bd0001efcf
jahnavee45 <roysimran1545@gmail.com>
2024-12-08 10:00:07 +0530

    new commit

--- MapAndSet/validAnagram.java
+++ MapAndSet/validAnagram.java
@@ -1,5 +1,7 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class validAnagram {
     public static void main(String[] args) {
         String s = "anagram";
@@ -11,6 +13,30 @@ public class validAnagram {
     public static boolean isAnagram(String s, String t){
         if(s.length() != t.length()){
             return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            }
+            else{
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            if(!map.containsKey(s.charAt(i))){
+                return false;
+            }else{
+                    map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+            }           
+        }
+
+        for(int count : map.values()){
+            if(count != 0){
+                return false;
+            }
         }
         return true;
     }
