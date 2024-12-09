e3422192b7664c5683665927ffb8e74690bd386b
jahnavee45 <roysimran1545@gmail.com>
2024-12-09 10:28:00 +0530

    new commit

--- GeneralUseCase/hello.java
+++ GeneralUseCase/hello.java
@@ -2,7 +2,7 @@ package GeneralUseCase;
 
 public class hello {
     public static void main(String[] args) {
-        String s = " hello";
+        String s = "hello!";
         System.out.println("Say " + s);
     }
 }
