271598a5172299aa1aee6028e1fbcd907870eab2
jahnavee45 <roysimran1545@gmail.com>
2024-12-10 09:25:12 +0530

    new commit

--- MapAndSet/countTargetSumSubArray.java
+++ MapAndSet/countTargetSumSubArray.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class countTargetSumSubArray {
     public static void main(String[] args) {
         int[] arr = {5, 7, 9, 13, 11, 6, 6, 3, 3};
-        int target = 12;
+        int target = 56;
         
         System.out.println("The number of distinct pairs of target sum " + target + " is " + findCount(arr, target));
     }
