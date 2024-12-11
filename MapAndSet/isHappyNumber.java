b785e5468c6f25cd146cc741b64aee8fe3d1cf9f
jahnavee45 <roysimran1545@gmail.com>
2024-12-11 09:21:25 +0530

    new commit

--- MapAndSet/isHappyNumber.java
+++ MapAndSet/isHappyNumber.java
@@ -4,23 +4,34 @@ import java.util.HashSet;
 
 public class isHappyNumber {
     public static void main(String[] args) {
-        int n = 19;
-        System.out.println(isHappy(n)?"Yes is happy number":"No is not happy number");
+        int n = 18;
+        System.out.println(isHappy(n) ? "Yes is happy number" : "No is not happy number");
     }
 
-    public static boolean isHappy(int n){
+    public static boolean isHappy(int n) {
         HashSet<Integer> set = new HashSet<>();
 
-        while(n != 0){
-            if(!set.contains(n)){
+        while (n != 0) {
+            if (!set.contains(n)) {
                 set.add(n);
-                n = getNumber(n){
-                    if(n == 1){
-                        return true;
-                    }
+                n = getNumber(n);
+                if (n == 1) {
+                    return true;
                 }
+
             }
         }
         return false;
+    }
+
+    public static int getNumber(int n) {
+        int ans = 0;
+
+        while (n != 0) {
+            int r = n % 10;
+            ans = r * r;
+            n /= 10;
+        }
+        return ans;
     }
 }
