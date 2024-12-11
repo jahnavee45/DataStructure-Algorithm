dde152f3c42d010328041baa0142cbe0685a3908
jahnavee45 <roysimran1545@gmail.com>
2024-12-11 09:18:37 +0530

    new commit

--- MapAndSet/isHappyNumber.java
+++ MapAndSet/isHappyNumber.java
@@ -1,8 +1,16 @@
 package MapAndSet;
 
+import java.util.HashSet;
+
 public class isHappyNumber {
     public static void main(String[] args) {
         int n = 19;
         System.out.println(isHappy(n)?"Yes is happy number":"No is not happy number");
+    }
+
+    public static boolean isHappy(int n){
+        HashSet<Integer> set = new HashSet<>();
+
+        return false;
     }
 }
