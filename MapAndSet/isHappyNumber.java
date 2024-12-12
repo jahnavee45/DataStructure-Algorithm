c4973a3198127d4b763b2ca23918a35e8a561e30
jahnavee45 <roysimran1545@gmail.com>
2024-12-12 11:55:11 +0530

    new commit

--- MapAndSet/isHappyNumber.java
+++ MapAndSet/isHappyNumber.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 
 public class isHappyNumber {
     public static void main(String[] args) {
-        int n = 2;
+        int n = 19;
         System.out.println(isHappy(n) ? "Yes is happy number" : "No is not happy number");
     }
 
---
+++ MapAndSet/majorityElement.java
@@ -0,0 +1,9 @@
+package MapAndSet;
+
+public class majorityElement {
+    public static void main(String[] args) {
+        int [] arr = {2, 2, 3, 2, 3};
+        int res = findMajority(arr);
+        System.out.println("The mejority element is " + res);
+    }
+}
