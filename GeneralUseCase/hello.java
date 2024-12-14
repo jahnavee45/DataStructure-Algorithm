3c47c18a287cfc9a80a2cebe0bc8e6f95b8ea52f
jahnavee45 <roysimran1545@gmail.com>
2024-12-14 10:43:59 +0530

    new commit

--- GeneralUseCase/hello.java
+++ GeneralUseCase/hello.java
@@ -1,8 +1,20 @@
 package GeneralUseCase;
 
+import java.util.HashMap;
+
 public class hello {
     public static void main(String[] args) {
         String s = "hello!";
         System.out.println("Say " + s);
+        
+        System.out.println("After Reversing: " + reverse(s));
+    }
+
+    public static String reverse(String s){
+        String str = null;
+
+        HashMap<Integer, Character> map = new HashMap<>();
+        
+        return str;
     }
 }
