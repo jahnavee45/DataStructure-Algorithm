b3243299c0e7b054822be5c0fc56ffbb7f59ac70
jahnavee45 <roysimran1545@gmail.com>
2024-12-21 10:51:24 +0530

    new commit

---
+++ GeneralUseCase/PredicateFuntionalInterface.java
@@ -0,0 +1,35 @@
+package GeneralUseCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateFuntionalInterface {
+
+    public static void main(String[] args) {
+        List<Employee> data = Arrays.asList(
+                new Employee(1, "John",10000),
+                new Employee(2, "Alice", 9000),
+                new Employee(3, "Bob", 8000));
+
+        List<Employee> newData = data.stream()
+                .filter(d -> d.getSalary() > 8000).collect(Collectors.toList());
+        for(Employee employee : newData) {
+            System.out.println(employee.getId());
+            System.out.println(employee.getName());
+            System.out.println(employee.getSalary());
+        }
+
+        // traditional way of using predicate functional interface
+        Predicate<Employee> predicate = (e) -> e.getName().startsWith("J");
+        List<Employee> newDataTraditional = data.stream()
+                .filter(predicate).collect(Collectors.toList());
+        for(Employee employee : newDataTraditional) {
+            System.out.println(employee.getId());
+            System.out.println(employee.getName());
+            System.out.println(employee.getSalary());
+        }
+    }
+}
+
