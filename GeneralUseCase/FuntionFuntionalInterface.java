30a60cc4a8cade0c9631465639644f1c02c0dd74
jahnavee45 <roysimran1545@gmail.com>
2024-12-21 10:52:30 +0530

    new commit

---
+++ GeneralUseCase/FuntionFuntionalInterface.java
@@ -0,0 +1,20 @@
+package GeneralUseCase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FuntionFuntionalInterface {
+    public static void main(String[] args) {
+        List<Integer> val = Arrays.asList(10, 20, 35);
+        List<Double> res = val.stream()
+                .map(v -> (double)v/2).collect(Collectors.toList());
+        System.out.println(res);
+
+        // traditional method using function functional interface
+        Function<Integer, Integer> v = x -> x*x;
+        Integer r = v.apply(10);
+        System.out.println(r);
+    }
+}
