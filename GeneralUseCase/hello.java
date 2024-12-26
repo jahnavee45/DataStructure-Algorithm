6a2728f7316fe816a452a315cafbcefc378f11b7
jahnavee45 <roysimran1545@gmail.com>
2024-12-26 21:09:44 +0530

    new commit

--- GeneralUseCase/hello.java
+++ GeneralUseCase/hello.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class hello {
     public static void main(String[] args) {
-        String s = "hello";
+        String s = "hello world";
         System.out.println("Say " + s);
         
         System.out.println("After reversing:" + reverse(s));
