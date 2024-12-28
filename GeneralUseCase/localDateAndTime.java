2c486254f27d928d376a9bcb6fc03e834ef51334
jahnavee45 <roysimran1545@gmail.com>
2024-12-28 11:40:19 +0530

    new commit

--- GeneralUseCase/localDateAndTime.java
+++ GeneralUseCase/localDateAndTime.java
@@ -1,8 +1,12 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class localDateAndTime {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         System.out.println("Local Date: " + date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println("Local Time: " + time);
     }
 }
