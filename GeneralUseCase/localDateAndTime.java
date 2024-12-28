bc682ad6e4b101c8cafbc4fc7cbd388c21623c20
jahnavee45 <roysimran1545@gmail.com>
2024-12-28 11:43:58 +0530

    new commit

--- GeneralUseCase/localDateAndTime.java
+++ GeneralUseCase/localDateAndTime.java
@@ -4,15 +4,20 @@ import java.time.ZonedDateTime;
 
 public class localDateAndTime {
     public static void main(String[] args) {
+
+        // to find local date
         LocalDate date = LocalDate.now();
         System.out.println("Local Date: " + date);
 
+        // to find local time
         LocalTime time = LocalTime.now();
         System.out.println("Local Time: " + time);
 
+        // to find date and time according to zone/area
         ZonedDateTime dt = ZonedDateTime.now();
         System.out.println("Zoned date and Time: " + dt);
 
+        // to print a given date
         LocalDate x = LocalDate.of(2024, 12, 31);
         System.out.println(x);
     }
