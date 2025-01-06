0f89e66687a8592823db8a7a7e8e0dd833461909
jahnavee45 <roysimran1545@gmail.com>
2025-01-06 10:00:32 +0530

    new commit

---
+++ Array/CountVowels.java
@@ -0,0 +1,13 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                count++;
+            }
+        }
+        System.out.println("Number of vowels in the given string is: " + count);
+    }
+    
+}
