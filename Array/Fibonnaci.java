01c98c2b744b022e84d0f86da641bbbe364c68d8
jahnavee45 <roysimran1545@gmail.com>
2025-01-06 09:56:28 +0530

    new commit

--- Array/Fibonnaci.java
+++ Array/Fibonnaci.java
@@ -1,7 +1,7 @@
 public class Fibonnaci {
     public static void main(String[] args) {
         int a = 0, b = 1;
-        int n = 5;
+        int n = 10;
         int res = findNthFibonacci(a, b, n);
         System.out.println("10th element of fibonacci series: " + res);
     }
