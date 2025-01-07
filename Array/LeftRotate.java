b5af4407b909710610113082bc44cc146c77a0fb
jahnavee45 <roysimran1545@gmail.com>
2025-01-07 10:47:42 +0530

    new commit

--- Array/LeftRotate.java
+++ Array/LeftRotate.java
@@ -2,13 +2,29 @@ public class LeftRotate {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30};
         int r = 2;
-
+        printArray(arr);
+        rotateArray(arr, r);
         printArray(arr);
     }
 
     public static void printArray(int [] arr){
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void rotateArray(int[] arr, int r){
+        int i = 0;
+        int [] arr1 = new int[arr.length];
+        for(int j = r-1; j < arr.length; j++){
+            arr1[i++] = arr[j];
+        }
+        for(int j = 0; j < r-1; j++){
+            arr1[i++] = arr[j];
+        }
+        for(int j = 0; j < arr1.length; j++){
+            arr[j] = arr1[j];
         }
     }
 }
