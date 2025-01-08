c79b541cbbde9a2b2a82026f0cf9b36c61a772ee
jahnavee45 <roysimran1545@gmail.com>
2025-01-08 10:30:37 +0530

    new commit

--- Array/EqualArrays.java
+++ Array/EqualArrays.java
@@ -17,12 +17,16 @@ public class EqualArrays {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i = 0; i < a1.length; i++){
-            map.put(a1[i], i);
+            if(map.containsKey(a1[i])){
+                map.put(a1[i], map.get(a1[i]) + 1);
+            }else{
+                map.put(a1[i], 1);
+            }
         }
 
         for(int i = 0; i < a2.length; i++){
             if(map.containsKey(a2[i])){
-                map.put(a2[i], 0);
+                map.put(a2[i], map.get(a2[i]) - 1);
             }else{
                 return false;
             }
