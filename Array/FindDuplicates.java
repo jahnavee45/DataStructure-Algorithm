dadae2e176726cfc910b8ef8db43ca8b2ccb7a03
jahnavee45 <roysimran1545@gmail.com>
2025-01-08 10:20:45 +0530

    new commit

---
+++ Array/FindDuplicates.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+public class FindDuplicates {
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2, 3, 4, 3, 3};
+        examine(a, a.length);
+    }
+
+    public static void examine(int[] arr, int n){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }else{
+                map.put(arr[i], 1);
+            }
+        }
+
+        for(int key : map.keySet()){
+            if(map.get(key) > 1){
+                System.out.println(key + " " + map.get(key));
+            }
+        }
+    }
+}
