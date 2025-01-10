f45bab97825ddb6f113173608e256bef86ffedf6
jahnavee45 <roysimran1545@gmail.com>
2025-01-10 09:41:56 +0530

    new commit

---
+++ Array/MissingNumber.java
@@ -0,0 +1,7 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+        int missing = findMissingNumber(arr, arr.length);
+    }
+    
+}
