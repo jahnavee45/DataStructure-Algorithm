e5b9c3e954cf64f28b1c3d186ac27924526f2896
jahnavee45 <roysimran1545@gmail.com>
2025-01-11 13:54:00 +0530

    new commit

---
+++ Array/CountDigits.java
@@ -0,0 +1,15 @@
+public class CountDigits {
+    public static void main(String[] args) {
+        int n = 234567, count = 0;
+        int res = findCount(n, count);
+        System.out.println("The number of digits in " + n + " is " + res);
+    }
+
+    public static int findCount(int n, int count){
+        if(n == 0){
+            return count;
+        }
+        n /= 10;
+        return findCount(n, count++);
+    }
+}
