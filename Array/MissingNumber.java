e6e6d5b677cbcf6acdfea7545525af39a48043a4
jahnavee45 <roysimran1545@gmail.com>
2025-01-11 13:47:30 +0530

    new commit

--- Array/MissingNumber.java
+++ Array/MissingNumber.java
@@ -1,6 +1,6 @@
 public class MissingNumber {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9, 10};
         int k = 10;
         int missing = findMissingNumber(arr, arr.length, k);
         System.out.println("Missing number is " + missing);
