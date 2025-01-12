3cdf4a8b85db5d52162ea71394acb9d2e46b7e01
jahnavee45 <roysimran1545@gmail.com>
2025-01-12 12:30:10 +0530

    new commit

--- Array/Matrix.java
+++ Array/Matrix.java
@@ -1,7 +1,13 @@
 public class Matrix {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {2, 3, 4}};
+        int n = 2, m = 3;
 
-        System.out.println("Matrix: " + arr);
+        System.out.println("Matrix: ");
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+        }
     }
 }
