21f3f29ca3a30f7ba0074dbe1cb84130c5b2ca6f
jahnavee45 <roysimran1545@gmail.com>
2025-01-24 10:11:50 +0530

    new commit

--- Array/MultiplicationMatrix.java
+++ Array/MultiplicationMatrix.java
@@ -1,12 +1,18 @@
 public class MultiplicationMatrix {
     public static void main(String[] args) {
-        int[][] a1 = {{1, 2, 3}, {2, 3, 4}};
-        int[][] a2 = {{1, 2, 3}, {2, 3, 4}};
+        int[][] a1 = {{1, 2}, {2, 3}};
+        int[][] a2 = {{1, 2}, {2, 3}};
         findMultiplication(a1, a2);
     }
 
     public static void findMultiplication(int[][] a1, int[][] a2){
-        int[][] a3 = new int[2][3];
-        
+        int prod = 0, k = 0;
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 3; j++){
+                prod = prod + (a1[i][j] * a2[i][j]);
+            }
+            System.out.println(prod);
+        }
+
     }
 }
