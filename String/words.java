0394b5ff4ce2c8cb2fce4ebae15565b2ebe80d0f
jahnavee45 <roysimran1545@gmail.com>
2025-01-26 09:17:26 +0530

    new commit

---
+++ String/words.java
@@ -0,0 +1,10 @@
+package String;
+
+public class words {
+    public static void main(String[] args) {
+        String st = "hello";
+        for(char c : st.toCharArray()){
+            System.out.print(c);
+        }
+    }
+}
