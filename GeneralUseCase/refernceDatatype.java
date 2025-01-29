cef16ca9940c7e62a69db4c4a68dd2002e6985fb
jahnavee45 <roysimran1545@gmail.com>
2025-01-29 10:07:52 +0530

    new commit

--- GeneralUseCase/refernceDatatype.java
+++ GeneralUseCase/refernceDatatype.java
@@ -1,33 +1,6 @@
-class Student{
-
-private String name;
-private int age;
-
-public Student(String name, int age){
-    this.name = name;
-    this.age = age;
-}
-
-public String getName(){
-    return name;
-}
-
-public void setName(String name){
-    this.name = name;
-}
-
-public int getAge(){
-    return age;
-}
-
-public void setAge(int age){
-    this.age = age;
-}
-}
-
 public class refernceDatatype {
     public static void main(String[] args) {
-        Student s1 = new Student("mike", 12);
+        StudentRD s1 = new StudentRD("mike", 12);
         System.out.println("The student is " + s1.getName() + " of age " + s1.getAge());
     }
 }
