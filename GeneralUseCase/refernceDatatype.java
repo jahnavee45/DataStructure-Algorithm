d1f77cf79a0216bc1f755cd3bb45708e4ab5ebc8
jahnavee45 <roysimran1545@gmail.com>
2025-01-30 16:22:12 +0530

    new commit

--- GeneralUseCase/refernceDatatype.java
+++ GeneralUseCase/refernceDatatype.java
@@ -1,6 +1,6 @@
 public class refernceDatatype {
     public static void main(String[] args) {
-        StudentRD s1 = new StudentRD("Stan", 12);
+        StudentRD s1 = new StudentRD("Stan", 14);
         System.out.println("The student is " + s1.getName() + " of age " + s1.getAge());
     }
 }
