a56f9435803469504b25edac8fb5efd6cede2e57
jahnavee45 <roysimran1545@gmail.com>
2025-02-01 09:26:59 +0530

    new commit

---
+++ GeneralUseCase/BankAccount.java
@@ -0,0 +1,9 @@
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+
+    public BankAccount(int accountNumber, double balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+}
