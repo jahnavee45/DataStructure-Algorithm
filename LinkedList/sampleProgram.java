e5cc2ab99a24ad47f8a9303f8895230a95b99e50
jahnavee45 <roysimran1545@gmail.com>
2025-02-06 11:51:14 +0530

    new commit

--- LinkedList/sampleProgram.java
+++ LinkedList/sampleProgram.java
@@ -1,5 +1,15 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
 public class sampleProgram {
     public static void main(String[] args) {
-        
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(24);
+        list.add(52);
+        list.add(38);
+        list.add(97);
+
+        System.out.println(list);
     }
 }
