787f25df2056f4a3a1ac417b3e9fd81be7b80eae
jahnavee45 <roysimran1545@gmail.com>
2025-02-10 10:19:36 +0530

    new commit

--- StreamApi/findSum.java
+++ StreamApi/findSum.java
@@ -1,5 +1,13 @@
 package StreamApi;
+import java.util.Arrays;
+import java.util.List;
 
 public class findSum {
-    
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 3, 5, 7, 9);
+
+        int sum = list.stream().mapToInt(Integer :: intValue).sum();
+
+        System.out.println("Sum of given list " + list + " is " + sum);
+    }
 }
