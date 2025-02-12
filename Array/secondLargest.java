06e364ac65d6aa0ff39c4747c16cb6d7118fcd27
jahnavee45 <roysimran1545@gmail.com>
2025-02-12 12:36:35 +0530

    new commit

--- Array/secondLargest.java
+++ Array/secondLargest.java
@@ -1,6 +1,9 @@
 public class secondLargest {
     public static void main(String[] args) {
         int[] arr = {27, 10, 34, 66, 3};
+
+        System.out.println("Array before sorting: ");
+
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr.length-1; j++){
                 if(arr[j] > arr[j+1]){
