4f32e30964b466fa1b57cea29ed612a3d7fb819d
jahnavee45 <roysimran1545@gmail.com>
2025-02-14 12:07:02 +0530

    new commit

--- StreamApi/reverseString.java
+++ StreamApi/reverseString.java
@@ -1,8 +1,14 @@
 package StreamApi;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class reverseString {
     public static void main(String[] args) {
         String str = "hello world";
-        
+        String rev = Stream.of(str)
+         .map(word -> new StringBuilder().reverse().toString()).collect(Collectors.joining(" "));
+
+        System.out.println("Reversed string: " + rev);
     }
 }
