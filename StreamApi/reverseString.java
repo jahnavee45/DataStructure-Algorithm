3f1c60edd9a7f3433f07748371b4c9a7445dfe7a
jahnavee45 <roysimran1545@gmail.com>
2025-02-16 15:16:42 +0530

    new commit

--- StreamApi/reverseString.java
+++ StreamApi/reverseString.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 
 public class reverseString {
     public static void main(String[] args) {
-        String str = "java is a high level programming language";
+        String str = "java is a programming language";
         System.out.println("Original string: " + str);
         String rev = Stream.of(str)
          .map(word -> new StringBuilder(word).reverse().toString()).collect(Collectors.joining(" "));
