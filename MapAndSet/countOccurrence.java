b8cc85611f614399a5f9e44e94ee98934e5cf3c4
jahnavee45 <roysimran1545@gmail.com>
2025-02-17 09:27:22 +0530

    new commit

--- MapAndSet/countOccurrence.java
+++ MapAndSet/countOccurrence.java
@@ -5,13 +5,15 @@ import java.util.HashMap;
 public class countOccurrence {
     public static void main(String[] args) {
         String str = "hello world";
-
+        
         HashMap<Character, Integer> map = new HashMap<>();
         for(char c : str.toCharArray()){
-            if(map.containsKey(c)){
-                map.put(c, map.get(c) + 1);
-            }else{
-                map.put(c, 1);
+            if(c != ' '){
+                if(map.containsKey(c)){
+                    map.put(c, map.get(c) + 1);
+                }else{
+                    map.put(c, 1);
+                }
             }
         }
 
