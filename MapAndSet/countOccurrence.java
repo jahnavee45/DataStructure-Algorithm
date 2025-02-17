a05486be77aac552fb4005e19dd4e3db618e343e
jahnavee45 <roysimran1545@gmail.com>
2025-02-17 09:19:01 +0530

    new commit

---
+++ MapAndSet/countOccurrence.java
@@ -0,0 +1,11 @@
+package MapAndSet;
+
+import java.util.HashMap;
+
+public class countOccurrence {
+    public static void main(String[] args) {
+        String str = "java";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+    }
+}
