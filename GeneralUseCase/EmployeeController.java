7f887f23c8a69cf98caf78c8ae2fc220965e18a3
jahnavee45 <roysimran1545@gmail.com>
2025-02-18 11:01:50 +0530

    new commit

--- GeneralUseCase/EmployeeController.java
+++ GeneralUseCase/EmployeeController.java
@@ -7,23 +7,23 @@ import java.util.stream.Collectors;
 public class EmployeeController {
     public static void main(String[] args) {
         ArrayList<Employee> list = new ArrayList<>();
-        list.add(new Employee(1, "mike","mike@gmail.com"));
-        list.add(new Employee(2, "smith","smith@gmail.com"));
-        list.add(new Employee(3, "stallin","stallin@gmail.com"));
-        list.add(new Employee(4, "jack","jack@gmail.com"));
+        list.add(new Employee(1, "mike", "mike@gmail.com"));
+        list.add(new Employee(2, "smith", "smith@gmail.com"));
+        list.add(new Employee(3, "stallin", "stallin@gmail.com"));
+        list.add(new Employee(4, "jack", "jack@gmail.com"));
 
         System.out.println("Employee details without sorting: ");
-        for(Employee e : list){
+        for (Employee e : list) {
             System.out.print(e.getId() + " " + e.getName() + " " + e.getEmail());
             System.out.println();
         }
 
         List<Employee> sortedList = list.stream()
-        .sorted(Comparator.comparing(Employee::getName))
-        .collect(Collectors.toList());
+                .sorted(Comparator.comparing(Employee::getName))
+                .collect(Collectors.toList());
 
         System.out.println("Employee details after sorting: ");
-        for(Employee e : sortedList){
+        for (Employee e : sortedList) {
             System.out.print(e.getId() + " " + e.getName() + " " + e.getEmail());
             System.out.println();
         }
