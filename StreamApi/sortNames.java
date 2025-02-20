566574eeb2278d977ee3bd25b5739f5f338b80d3
jahnavee45 <roysimran1545@gmail.com>
2025-02-20 10:08:54 +0530

    new commit

--- StreamApi/sortNames.java
+++ StreamApi/sortNames.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class sortNames {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Alice", "Charlie", "Dennis", "Bob");
-        
+        System.out.println(list);
+        List<String> sortedList = list.stream()
     }
 }
