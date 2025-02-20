a32e34a83bf555e764bc44272bba3f17efc06251
jahnavee45 <roysimran1545@gmail.com>
2025-02-20 10:05:47 +0530

    new commit

--- StreamApi/countStringOccurrence.java
+++ StreamApi/countStringOccurrence.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class countStringOccurrence {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Alice", "Bob","Charlie","Alice","Alice", "Charlie");
+        List<String> list = Arrays.asList("Alice", "Bob","Charlie","Alice", "Alice", "Dennis", "Charlie", "Dennis");
 
         Map<String, Long> countList = list.stream()
         .collect(Collectors.groupingBy(name -> name, Collectors.counting()));
