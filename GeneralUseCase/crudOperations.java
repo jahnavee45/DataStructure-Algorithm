c92bb8c50da2463eb14bfc9ee0b0ca9d5fe26ddd
jahnavee45 <roysimran1545@gmail.com>
2025-02-21 11:00:46 +0530

    new commit

--- GeneralUseCase/crudOperations.java
+++ GeneralUseCase/crudOperations.java
@@ -5,14 +5,14 @@ import java.util.List;
 public class crudOperations {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(2, 3, 4, 5);
-        List<Integer> newList = new ArrayList<>();
         System.out.println("Printing list: " + list);
+
+        List<Integer> newList = new ArrayList<>();
         for(int i = 0; i < list.size(); i++){
             if(list.get(i) != 4){
                 newList.add(list.get(i));
             }
         }
-
         System.out.println("After deletion: " + newList);
     }
 }
