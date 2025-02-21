6d1cd74e066d92ed2fb2f9a896ca1603a4f5e553
jahnavee45 <roysimran1545@gmail.com>
2025-02-21 10:54:56 +0530

    new commit

---
+++ GeneralUseCase/crudOperations.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class crudOperations {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(2, 3, 4, 5);
+        System.out.println("Printing list: " + list);
+    }
+}
