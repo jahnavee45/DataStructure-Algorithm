d1374d865a4e9b44a9a1bd990002df5c47522173
jahnavee45 <roysimran1545@gmail.com>
2025-02-22 11:21:31 +0530

    new commit

--- GeneralUseCase/crudOperations.java
+++ GeneralUseCase/crudOperations.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class crudOperations {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(2, 3, 4, 5);
+        List<Integer> list = Arrays.asList(2, 3, 4, 5, 6);
         System.out.println("Printing list: " + list);
 
         List<Integer> newList = new ArrayList<>();
