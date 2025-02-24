99c307e6c41cc82fd76d5d5cc95829d0c3067b0e
jahnavee45 <roysimran1545@gmail.com>
2025-02-24 10:25:57 +0530

    new commit

--- StreamApi/PredicateFuntionalInterface.java
+++ StreamApi/PredicateFuntionalInterface.java
@@ -4,17 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 public class PredicateFuntionalInterface {
 
     public static void main(String[] args) {
         List<Employee> data = Arrays.asList(
-                new Employee(1, "John",10000),
+                new Employee(1, "John", 10000),
                 new Employee(2, "Alice", 9000),
                 new Employee(3, "Bob", 8000));
 
         List<Employee> newData = data.stream()
                 .filter(d -> d.getSalary() > 8000).collect(Collectors.toList());
-        for(Employee employee : newData) {
+        for (Employee employee : newData) {
             System.out.print(employee.getId() + " " + employee.getName() + " " + employee.getSalary());
             System.out.println();
         }
@@ -23,9 +24,13 @@ public class PredicateFuntionalInterface {
         Predicate<Employee> predicate = (e) -> e.getName().startsWith("J");
         List<Employee> newDataTraditional = data.stream()
                 .filter(predicate).collect(Collectors.toList());
-        for(Employee employee : newDataTraditional) {
+        for (Employee employee : newDataTraditional) {
             System.out.print(employee.getId() + " " + employee.getName() + " " + employee.getSalary());
         }
+
+        // second largest salary
+        List<Employee> sortSalary = data.stream()
+                .sorted((e1, e2) -> Integer.compare(e1.getSalary(), e2.getSalary()))
+                .collect(Collectors.toList());
     }
 }
-
