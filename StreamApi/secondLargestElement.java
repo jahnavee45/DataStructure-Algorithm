05c0f541d7f169b28a5ddab918bda2f1824bcbd5
jahnavee45 <roysimran1545@gmail.com>
2025-03-02 09:14:17 +0530

    new commit

--- StreamApi/secondLargestElement.java
+++ StreamApi/secondLargestElement.java
@@ -4,5 +4,15 @@ import java.util.List;
 public class secondLargestElement {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(3);
+        list.add(4);
+        list.add(0);
+
+        System.out.println("Before sorting: " + list);
     }
 }
