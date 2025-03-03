fe5063c7d89c12fe18caf26b9b2cda6210f4f633
jahnavee45 <roysimran1545@gmail.com>
2025-03-03 09:28:45 +0530

    new commit

--- MapAndSet/evenOdd.java
+++ MapAndSet/evenOdd.java
@@ -1,19 +1,21 @@
 package MapAndSet;
 
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class evenOdd {
     public static void main(String[] args) {
        int[] arr = {3, 42, 72, 11, 59, 60};
        
-       HashMap<String, Integer> map = new HashMap<>();
+       HashSet<Integer> even = new HashSet<>();
+       HashSet<Integer> odd = new HashSet<>();
        for(int i = 0; i < arr.length; i++) {
         if(arr[i] % 2 == 0){
-            map.put("Even", arr[i]);
-        }
-        else{
-            map.put("Odd", arr[i]);
+            even.add(arr[i]);
+        }else{
+            odd.add(arr[i]);
         }
        }
+
+       System.out.println();;
     }
 }
