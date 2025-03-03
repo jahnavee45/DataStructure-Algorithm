9033006209718af9db0921505af8810a42c5fff6
jahnavee45 <roysimran1545@gmail.com>
2025-03-03 09:22:26 +0530

    new commit

--- MapAndSet/evenOdd.java
+++ MapAndSet/evenOdd.java
@@ -1,7 +1,12 @@
 package MapAndSet;
 
+import java.util.HashMap;
+
 public class evenOdd {
     public static void main(String[] args) {
-        
+       int[] arr = {3, 42, 72, 11, 59, 60};
+       
+       HashMap<String, Integer> map = new HashMap<>();
+       
     }
 }
