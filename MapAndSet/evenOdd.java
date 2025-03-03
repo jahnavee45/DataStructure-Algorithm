91fece713f81f2911892fbc038155ce1ef28ccdb
jahnavee45 <roysimran1545@gmail.com>
2025-03-03 09:24:14 +0530

    new commit

--- MapAndSet/evenOdd.java
+++ MapAndSet/evenOdd.java
@@ -7,6 +7,13 @@ public class evenOdd {
        int[] arr = {3, 42, 72, 11, 59, 60};
        
        HashMap<String, Integer> map = new HashMap<>();
-       
+       for(int i = 0; i < arr.length; i++) {
+        if(arr[i] % 2 == 0){
+            map.put("Even", arr[i]);
+        }
+        else{
+            map.put("Odd", arr[i]);
+        }
+       }
     }
 }
