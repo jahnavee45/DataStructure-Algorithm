898c2fe36be46ac591a5e72469e2100795959227
jahnavee45 <roysimran1545@gmail.com>
2025-03-04 10:49:09 +0530

    new commit

--- StreamApi/evenOdd.java
+++ StreamApi/evenOdd.java
@@ -10,7 +10,8 @@ public class evenOdd {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 7);
         System.out.println("Given list is: " + list);
 
-        Map<Boolean, List<Integer>> check = list.stream().collect(Collectors.partitioningBy(n -> n % 2 == 0));
+        Map<Boolean, List<Integer>> check = list.stream()
+                .collect(Collectors.partitioningBy(n -> n % 2 == 0));
 
         System.out.println("List after partitioning even and odd: " + check);
 
@@ -20,8 +21,13 @@ public class evenOdd {
         System.out.println("Even numbers are: " + even);
         System.out.println("Odd numbers are: " + odd);
 
-        List<Integer> evenNum = list.stream().filter(e -> e%2 == 0).collect(Collectors.toList());
-        List<Integer> oddNum = list.stream().filter(e -> e%2 != 0).collect(Collectors.toList());
+        List<Integer> evenNum = list.stream()
+        .filter(e -> e % 2 == 0)
+        .collect(Collectors.toList());
+
+        List<Integer> oddNum = list.stream()
+        .filter(e -> e % 2 != 0)
+        .collect(Collectors.toList());
 
         System.out.println("Even numbers: " + evenNum);
         System.out.println("Odd numbers: " + oddNum);
