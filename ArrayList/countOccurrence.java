74ab137a728da4eed5ad8684da6b624e500d3ff5
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-05 10:56:00 +0530

    new commit

--- ArrayList/countOccurrence.java
+++ ArrayList/countOccurrence.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+
 public class countOccurrence {
     private static final String List = null;
     
@@ -17,6 +19,24 @@ public class countOccurrence {
     }
 
     public static ArrayList<Integer> majorityEle(ArrayList<Integer> arr){
-        
+        int floor = (int)Math.floor(arr.size()/3);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.size(); i++){
+            if(map.containsKey(arr.get(i))){
+                map.put(arr.get(i), map.get(arr.get(i)) + 1);
+            }else{
+                map.put(arr.get(i), 1);
+            }
+        }
+
+        for(int v : map.keySet()){
+            if(map.get(v) > floor){
+                list.add(v);
+            }
+        }
+        return list;
     }
 }
