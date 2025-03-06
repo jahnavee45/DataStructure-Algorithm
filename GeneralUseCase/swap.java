a9593976b8f55385b998af5318e0a9b87b7e08a5
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-06 15:26:23 +0530

    new commit

--- GeneralUseCase/swap.java
+++ GeneralUseCase/swap.java
@@ -2,10 +2,11 @@ public class swap {
     public static void main(String[] args) {
         int a = 78;
         int b = 67;
-
         System.out.println("Before swapping a = " + a + " and b = " + b);
-        a = a + b - a;
+        
+        a = a + b;
         b = a - b;
+        a = a - b;
         System.out.println("After swapping a = " + a + " and b = " + b);
 
     }
