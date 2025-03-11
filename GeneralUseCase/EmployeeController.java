381056ef9950fe842590a85e75dc070dffe490c1
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-11 10:34:20 +0530

    new commit

--- GeneralUseCase/EmployeeController.java
+++ GeneralUseCase/EmployeeController.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class EmployeeController {
@@ -9,12 +10,13 @@ public class EmployeeController {
         list.add(new Employee(1, "mike", "mike@gmail.com", 18000, "male"));
         list.add(new Employee(2, "smith", "smith@gmail.com", 18000, "male"));
         list.add(new Employee(3, "jane", "jane@gmail.com", 30000, "female"));
-        list.add(new Employee(4, "jack", "jack@gmail.com",  22000, "male"));
-        list.add(new Employee(5, "april", "april@gmail.com",  25000, "female"));
+        list.add(new Employee(4, "jack", "jack@gmail.com", 22000, "male"));
+        list.add(new Employee(5, "april", "april@gmail.com", 25000, "female"));
 
         System.out.println("Employee details without sorting: ");
         for (Employee e : list) {
-            System.out.print(e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
+            System.out.print(
+                    e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
             System.out.println();
         }
         System.out.println();
@@ -25,17 +27,19 @@ public class EmployeeController {
 
         System.out.println("Employee details after sorting according to names: ");
         for (Employee e : sortedList) {
-            System.out.print(e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
+            System.out.print(
+                    e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
             System.out.println();
         }
 
         List<Employee> secondLargestSalary = list.stream()
-        .sorted((e1, e2) -> Integer.compare(e2.getSalary(), e1.getSalary()))
-        .collect(Collectors.toList());
+                .sorted((e1, e2) -> Integer.compare(e2.getSalary(), e1.getSalary()))
+                .collect(Collectors.toList());
 
-        if(secondLargestSalary.size() > 1){
+        if (secondLargestSalary.size() > 1) {
             Employee e = secondLargestSalary.get(1);
-            System.out.println("\nThe second largest salary is of " + e.getName() + " with " + e.getSalary() + " " + e.getGender());
+            System.out.println("\nThe second largest salary is of " + e.getName() + " with " + e.getSalary() + " "
+                    + e.getGender());
         }
         System.out.println();
         List<Employee> findName = list.stream()
@@ -44,8 +48,15 @@ public class EmployeeController {
 
         System.out.println("Employee details starting with letter s: ");
         for (Employee e : findName) {
-            System.out.print(e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
+            System.out.print(
+                    e.getId() + " " + e.getName() + " " + e.getEmail() + " " + e.getSalary() + " " + e.getGender());
             System.out.println();
         }
+
+        System.out.println("Number of male and female employees: ");
+        Map<String, Long> map = list.stream()
+                .collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+        System.out.println(map);
+
     }
 }
