1459cbf6bd171144014c972a0d9ec8b9508553ae
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-11 10:26:41 +0530

    new commit

--- GeneralUseCase/Employee.java
+++ GeneralUseCase/Employee.java
@@ -3,12 +3,14 @@ public class Employee {
     private String name;
     private String email;
     private int salary;
+    private String gender;
 
-    public Employee(int id, String name, String email, int salary){
+    public Employee(int id, String name, String email, int salary, String gender){
         this.id = id;
         this.name = name;
         this.email = email;
         this.salary = salary;
+        this.gender = gender;
     }
 
     public int getId(){
@@ -25,5 +27,9 @@ public class Employee {
 
     public int getSalary(){
         return salary;
+    }
+
+    public String getGender(){
+        return gender;
     }
 }
