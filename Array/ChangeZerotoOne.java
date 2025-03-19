0c7904f1a73d9c76c755042c6a24132889a34b67
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-19 12:10:59 +0530

    new commit

--- Array/ChangeZerotoOne.java
+++ Array/ChangeZerotoOne.java
@@ -1,5 +1,10 @@
 public class ChangeZerotoOne {
     public static void main(String[] args) {
         int[] arr = {1, 0, 1, 0, 1, 1};
+
+        System.out.println("Before converting: ");
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
     }
 }
