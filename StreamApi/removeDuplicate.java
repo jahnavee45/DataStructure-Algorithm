acd58eb80198cd5863a0b49a7da5c4b101d14f67
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-28 10:58:46 +0530

    new commit

---
+++ StreamApi/removeDuplicate.java
@@ -0,0 +1,11 @@
+package StreamApi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 3};
+        
+    }
+}
