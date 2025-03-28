e9e6529407f34cb2d79de04aeb1a9221f7fca8d4
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-03-28 11:02:06 +0530

    new commit

--- StreamApi/removeDuplicate.java
+++ StreamApi/removeDuplicate.java
@@ -7,7 +7,8 @@ import java.util.stream.Collectors;
 public class removeDuplicate {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 2, 3, 4, 3);
-        
+        System.out.println("Before removing duplicates: " + list);
+
         List<Integer> list1 = list.stream()
         .distinct()
         .collect(Collectors.toList());
