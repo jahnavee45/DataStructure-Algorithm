4b72db336790e0e08f8f6dfd4133bc1b635ffd54
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-01 10:20:46 +0530

    new commit

--- String/reverseEachWordString.java
+++ String/reverseEachWordString.java
@@ -4,6 +4,17 @@ public class reverseEachWordString {
     }
 
     public static void reverseWord(String str){
-        
+        String [] words = str.split("//s");
+        String revStr = "";
+        for(String word : words){
+            String revWord = "";
+            for(int j = word.length()-1; j >= 0; j--){
+                revWord += word.charAt(j);
+            }
+            revStr += revWord + " ";
+        }
+
+        System.out.println(str);
+        System.out.println(revStr);
     }
 }
