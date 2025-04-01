08625ba3b37400a07fc1c824ab26eae8d32c30f9
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-01 10:15:58 +0530

    new commit

---
+++ String/reverseEachWordString.java
@@ -0,0 +1,9 @@
+public class reverseEachWordString {
+    public static void main(String[] args) {
+        reverseWord("Hello World");
+    }
+
+    public static void reverseWord(String str){
+        
+    }
+}
