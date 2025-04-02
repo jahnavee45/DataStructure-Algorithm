f67022a787cbd0c1251c059a7a87aeefda3a0bdc
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-02 09:29:22 +0530

    new commit

--- String/lexicographicOrder.java
+++ String/lexicographicOrder.java
@@ -2,12 +2,42 @@ public class lexicographicOrder {
     public static void main(String[] args) {
         String[] str = {"bde","dcf", "ght"};
         int count = countUnsortedOrder(str);
+        
     }
 
     public static int countUnsortedOrder(String[] str){
         int m = str.length;
-        String s = str[0];
-        int n = s.length();
-        
+        String st = str[0];
+        int n = st.length();
+
+        char[][] ch = new char[m][n]; 
+        for(String s: str){
+            if(s.length() != n){
+                return 0;
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            String word = str[i];
+            for(int j = 0; j < n; j++){
+                ch[i][j] = word.charAt(j);
+            }
+        }
+
+        int count = 0;
+        for(int j = 0; j < n; j++){
+            boolean isSorted = true;
+            for(int i = 0; i < m; i++){
+                if(ch[i][j] > ch[i+1][j]){
+                    isSorted = false;
+                    break;
+                }
+            }
+            if(!isSorted){
+                count++;
+            }
+        }
+
+        return count;
     }
 }
