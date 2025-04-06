658a241bdca0bedf1135c3582faddbecd2b938e6
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-06 14:34:20 +0530

    new commit

--- String/lexicographicOrder.java
+++ String/lexicographicOrder.java
@@ -2,7 +2,7 @@ public class lexicographicOrder {
     public static void main(String[] args) {
         String[] str = {"bde","dcf", "ght"};
         int count = countUnsortedOrder(str);
-        System.out.println(count);
+        System.out.println("Number of unsorted orders: " + count);
     }
 
     public static int countUnsortedOrder(String[] str){
