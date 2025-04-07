323de64895f3b984886cfbaf85f0241d90f778f5
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-07 11:10:35 +0530

    new commit

---
+++ Array/largestDistinctElement.java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+
+public class largestDistinctElement {
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 3, 3, 9, 4, 2};
+        // int n = findThreeLargest(arr);
+    }
+}
