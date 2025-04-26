018653f8184e89c9a4a6a47e2a363407eb993f30
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-26 11:29:57 +0530

    new commit

--- Array/convertIntegerToBinary.java
+++ Array/convertIntegerToBinary.java
@@ -1,14 +1,15 @@
 public class convertIntegerToBinary {
     public static void main(String[] args) {
         int num = 7;
-
         StringBuilder res= new StringBuilder();
+
+        System.out.print("Integer: " + num + " after reversing: ");
         while(num != 0){
             int rem = num % 2;
             res.append(rem);
             num /= 2;
         }
 
-        System.out.println(res.reverse().toString());
+        System.out.print(res.reverse().toString());
     }
 }
