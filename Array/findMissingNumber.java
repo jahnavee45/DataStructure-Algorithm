7c2ecc7549682e0cb3cf475d0b05636a15a333fe
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-28 12:37:18 +0530

    new commit

--- Array/findMissingNumber.java
+++ Array/findMissingNumber.java
@@ -11,7 +11,7 @@ public class findMissingNumber {
         return tsum - asum;
     }
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Missing number: " + findNumber(arr));
 
