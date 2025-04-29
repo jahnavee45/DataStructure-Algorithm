514ae4e1c9e570db6936385d78a60945b464c27d
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-29 09:43:33 +0530

    new commit

--- Array/CountVowels.java
+++ Array/CountVowels.java
@@ -1,6 +1,6 @@
 public class countVowels {
     public static void main(String[] args) {
-        String str = "Hello World in java";
+        String str = "Hello World";
         int count = 0;
         for(int i = 0; i < str.length(); i++){
             if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
