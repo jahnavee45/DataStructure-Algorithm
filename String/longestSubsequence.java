5a33b2b0411314169906cab2f5283650da5ffe4f
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-30 15:19:17 +0530

    new commit

--- String/longestSubsequence.java
+++ String/longestSubsequence.java
@@ -1,11 +1,24 @@
+import java.util.HashMap;
+
 public class longestSubsequence {
     public static void main(String[] args) {
-        String str ="abcabc";
+        String str ="abcbcabc";
         String res = findLongestSubsequence(str);
         System.out.println(res);
     }
 
     public static String findLongestSubsequence(String str){
-        
+        String res = "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < str.length(); i++){
+            if(map.containsKey(str.charAt(i))){
+                break;
+            }
+            else{
+                map.put(str.charAt(i), 1);
+                res += str.charAt(i);
+            }
+        }
+        return res;
     }
 }
