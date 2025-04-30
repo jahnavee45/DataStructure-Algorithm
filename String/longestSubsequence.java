eff128937dc847e2b4fcab1336ccac9689495020
roysimran1545@gmail.com <roysimran1545@gmail.com>
2025-04-30 15:14:18 +0530

    new commit

---
+++ String/longestSubsequence.java
@@ -0,0 +1,11 @@
+public class longestSubsequence {
+    public static void main(String[] args) {
+        String str ="abcabc";
+        String res = findLongestSubsequence(str);
+        System.out.println(res);
+    }
+
+    public static String findLongestSubsequence(String str){
+        
+    }
+}
